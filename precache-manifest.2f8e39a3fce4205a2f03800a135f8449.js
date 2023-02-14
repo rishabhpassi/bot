4d1d8b6082adae3610fbbfca6310757992c2d754
rishabhpassi <rishabh.passi199@gmail.com>
2023-02-13 21:22:47 -0800

    Updates

---
+++ precache-manifest.2f8e39a3fce4205a2f03800a135f8449.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "38c87daf528892ebcc5b",
+    "url": "/bot/static/css/main.10e1760b.chunk.css"
+  },
+  {
+    "revision": "38c87daf528892ebcc5b",
+    "url": "/bot/static/js/main.38c87daf.chunk.js"
+  },
+  {
+    "revision": "92bf2f06d2a0d81614c3",
+    "url": "/bot/static/js/1.92bf2f06.chunk.js"
+  },
+  {
+    "revision": "6112b14b1b49de7d7cc4",
+    "url": "/bot/static/js/2.6112b14b.chunk.js"
+  },
+  {
+    "revision": "5bc5f1cbaf754f8b0b44",
+    "url": "/bot/static/js/runtime~main.5bc5f1cb.js"
+  },
+  {
+    "revision": "a163e882f64da4a3479fa4e2db596824",
+    "url": "/bot/index.html"
+  }
+];
